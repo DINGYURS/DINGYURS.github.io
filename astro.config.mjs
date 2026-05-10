48269a3cacb34926be2608cb8035b6187617fa3e
DINGYU <DINGYURS@qq.com>
2026-05-10 17:03:04 +0800

    configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://dingyurs.github.io',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
